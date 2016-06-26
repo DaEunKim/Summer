2545f4a87b48dd61c5b7c75ebe313cc3544e257c
DaEunKim <Dani@gimda-eunui-macbook-air.local>
2016-06-26 23:53:38 +0900

    Initial Commit

---
+++ Summer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Summer
+//
+//  Created by 김다은 on 2016. 6. 26..
+//  Copyright © 2016년 김다은. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
