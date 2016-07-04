5e0c558d7b0b2aa28ca493ce6ab6d9e5b9efe809
DaEunKim <Dani@gimda-eunui-MacBook-Air.local>
2016-07-04 15:44:49 +0900

    .

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Summer
+//
+//  Created by 김다은 on 2016. 6. 26..
+//  Copyright © 2016년 김다은. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+
+int main(void){
+    int N;
+    cin >> N;
+    for(int i=1;i<10;i++){
+        cout<< N<<" * "<<i<<" = "<<N*i<<endl;
+
+        
+    }
+    
+}
---
+++ max.cpp
@@ -0,0 +1,9 @@
+//
+//  max.cpp
+//  Summer
+//
+//  Created by 김다은 on 2016. 7. 4..
+//  Copyright © 2016년 김다은. All rights reserved.
+//
+
+#include <stdio.h>
