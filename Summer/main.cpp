8d6c8d2ee3783a5e788b48b86a774f0aad879fc2
DaEunKim <Dani@gimda-eunui-MacBook-Air.local>
2016-07-04 15:31:45 +0900

    c++

--- Summer/main.cpp
+++ Summer/main.cpp
@@ -7,9 +7,15 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+int main(void){
+    int A;
+    int B;
+    cin >> A >> B;
+    cout<<A+B<<endl;
+    
 }
